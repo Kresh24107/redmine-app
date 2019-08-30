cad3113671105056d0167d164843f6ce933af8f2
Vadim Sirenko <vadim7ko@gmail.com>
2019-08-30 16:38:35 +0300

    for code review

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 In the project directory, you can run:
 
 ### `npm start`
+
+Demo on [GH-Pages](http://Kresh24107.github.io/redmine-app).
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <h1>redmine App</h1>;
+import RedmineList from "./components/RedmineList.jsx";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const promise = fetch(this.props.urlData, {
+      credentials: "include"
+    });
+
+    promise
+      .then(res => res.json())
+      .then(response =>
+        this.setState(() => ({
+          data: response
+        }))
+      );
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.data.length !== 0 && (
+          <RedmineList projects={this.state.data} />
+        )}
+      </>
+    );
+  }
 }
-
-export default App;
